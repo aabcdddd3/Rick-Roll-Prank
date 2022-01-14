4e320ae57fdaeb2f842b23f4900fe0cdd565b76a
Shubu909 <95223840+Shubu909@users.noreply.github.com>
2022-01-14 12:13:11 +0530

    Add files via upload

---
+++ rickrollprankmsedge.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+import pyttsx3
+
+driverroll = webdriver.Edge(executable_path="") # <-- Enter the location of your webdriver and don't forget to add double backslashes.
+speakroll = pyttsx3.init()
+driverroll.minimize_window()
+print("This is a prank made by Shubu909 and he's the respective owner of this file and coded this.")
+speakroll.say("This is a prank made by Shubu909 and he's the respective owner of this file and coded this.")
+speakroll.runAndWait()
+speakroll.say("GOT NECK! GOT FUCKING NECK! OH NECK!")
+driverroll.maximize_window()
+driverroll.minimize_window()
+driverroll.fullscreen_window()
+driverroll.minimize_window()
+driverroll.get("https://www.youtube.com/watch?v=graKroRK8Hg")
+element = driverroll.find_element_by_xpath("//*[@id='movie_player']/div[29]/div[2]/div[1]/button").click()
+speakroll.say("We're no strangers to love You know the rules and so do I A full commitment's what I'm thinking of You wouldn't get this from any other guy I just wanna tell you how I'm feeling Gotta make you understand Never gonna give you up Never gonna let you down Never gonna run around and desert you Never gonna make you cry Never gonna say goodbye Never gonna tell a lie and hurt you We've known each other for so long Your heart's been aching but you're too shy to say it Inside we both know what's been going on We know the game and we're gonna play it And if you ask me how I'm feeling Don't tell me you're too blind to see Never gonna give you up Never gonna let you down Never gonna run around and desert you Never gonna make you cry Never gonna say goodbye Never gonna tell a lie and hurt you Never gonna give you up Never gonna let you down Never gonna run around and desert you Never gonna make you cry Never gonna say goodbye Never gonna tell a lie and hurt you Never gonna give, never gonna give We've known each other for so long Your heart's been aching but you're too shy to say it Inside we both know what's been going on We know the game and we're gonna play it I just wanna tell you how I'm feeling Gotta make you understand Never gonna give you up Never gonna let you down Never gonna run around and desert you Never gonna make you cry Never gonna say goodbye Never gonna tell a lie and hurt you Never gonna give you up Never gonna let you down Never gonna run around and desert you Never gonna make you cry Never gonna say goodbye Never gonna tell a lie and hurt you Never gonna give you up Never gonna let you down Never gonna run around and desert you Never gonna make you cry Never gonna say goodbye")
+speakroll.runAndWait()
+speakroll.say("HAHAHAH! GOTTIEM DEEZ NUTS RICK ROLL NUTS!")
+speakroll.runAndWait()
+print("Thanks for using the Rick Roll Prank now it's ended so your free!")
+speakroll.say("Thanks for using the Rick Roll Prank now it's ended so your free!")
+speakroll.runAndWait()
